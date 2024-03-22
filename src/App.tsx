cc0b3ab37b1cae43c7f4fe909655d41a1b197183
unknown <maryemsmadhi@gmail.com>
2024-03-23 00:33:54 +0100

    changes in width of the content

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
  
- <ForgotPass></ForgotPass>
+ <Messenging></Messenging>
 
     </div>
   );
--- src/Pages/Messenging/index.tsx
+++ src/Pages/Messenging/index.tsx
@@ -4,16 +4,17 @@ import Button from '../../Components/Button'
 import YellowBand from '../../Components/YellowBand';
 import Inbox from '../../Components/ChatBar/Inbox';
 import threedots from '../../Assets/Images/threedots.png'
-import NavBar from '../../Components/NavBar/NavBar';
 import ChatRoom from '../../Components/ChatRoom/ChatRoom';
 import RightSideBar from '../../Components/RightSideBar';
 export default function Messenging(){
     return(
-        <div  className="flex items-center space-x-4" >
+        <div  className="flex items-center min-h-screen space-container w-screen mx-h-640 mx-autox-4"  style={{zoom:'0.8'}} >
            <div  style={{ width: '98px', height: '1080px' }} >
         <SideBar />
         </div>
         <div style={{
+            maxWidth:'368px',
+    height:'1080px',
     display: 'flex',
     padding: '10px 15px',
     flexDirection: 'column',
@@ -73,8 +74,12 @@ export default function Messenging(){
 <div className="ChatBar">
     
 </div>
-<div ><ChatRoom ></ChatRoom></div>
-<div><RightSideBar></RightSideBar></div>
+<div
+style={{maxWidth:"1920px",
+height:"1080px"}} ><ChatRoom ></ChatRoom></div>
+<div
+style={{maxWidth:"386px",
+height:"1080px"}}><RightSideBar></RightSideBar></div>
 
 </div>
       
